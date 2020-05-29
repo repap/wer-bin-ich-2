7b0362c54435cad3c5b3beee83d7ec4b76bc1bc5
Dennis Penning <penning.dennis@googlemail.com>
2020-05-29 06:47:11 +0200

    Add start game action

--- src/components/lobby/Lobby.jsx
+++ src/components/lobby/Lobby.jsx
@@ -2,8 +2,13 @@ import React, { Fragment } from 'react'
 import { PlayerList } from '../../presentations/playerList/PlayerList'
 import { CopyLink } from '../../presentations/copyLink/CopyLink'
 import { CreateAlias } from '../createAlias/CreateAlias'
+import { gameStateChangeAction } from '../../actions/game'
+import { AppState } from '../app/App'
+import { STATES } from '../../const/game'
 
 export const Lobby = () => {
+  const { dispatch } = React.useContext(AppState);
+  const startGame = () => dispatch(gameStateChangeAction(STATES.GAME_PLAY))
   return (
     <Fragment>
       <h2>Lobby</h2>
@@ -13,7 +18,7 @@ export const Lobby = () => {
         <h3>Mitspieler</h3>
         <PlayerList />
         <CreateAlias />
-        <button>So, kann jetzt losgehen!</button>
+        <button onClick={startGame}>So, kann jetzt losgehen!</button>
       </div>
     </Fragment>
   )
