d1451a4ba1a0f1f20e0642e5263ce939949a2eef
Dennis Penning <penning.dennis@googlemail.com>
2020-05-29 06:32:55 +0200

    First game state action

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useReducer } from 'react';
 import './App.css';
 import { Default } from '../../layouts/Default';
 import { Login } from '../login/Login';
@@ -7,18 +7,56 @@ import { Lobby } from '../lobby/Lobby';
 import { Game } from '../game/Game';
 import { Result } from '../result/Result';
 
+export const AppState = createContext()
+export const ACTION_TYPES = {
+  CHANGE_GAME_STATE: 'changeGameState',
+}
+
+export const STATES = {
+  PLAYER_CREATION: 'playerCreation',
+  GAME_CREATION: 'gameCreation',
+  GAME_PREPERATION: 'gamePreperation',
+  GAME_PLAY: 'gamePlay',
+  GAME_END: 'gameEnded',
+}
+
+const VIEW_STATE_MAP = {
+  [STATES.PLAYER_CREATION]: CreateOwnPlayer,
+  [STATES.GAME_CREATION]: Login,
+  [STATES.GAME_PREPERATION]: Lobby,
+  [STATES.GAME_PLAY]: Game,
+  [STATES.GAME_END]: Result,
+}
+
+const initialState = {
+  gameState: STATES.GAME_CREATION,
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.CHANGE_GAME_STATE:
+      return {
+        ...state,
+        gameState: action.gameState,
+      }
+    default:
+      return state
+  }
+}
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const GameStateComponent = VIEW_STATE_MAP[state.gameState]
   return (
-    <div className="App">
-      <Default>
-        <Login />
-        <CreateOwnPlayer />
-        <Lobby />
-        <Game />
-        <Game activePlayer />
-        <Result />
-      </Default>
-    </div>
+    <AppState.Provider
+      value={{state, dispatch}}
+    >
+      <div className="App">
+        <Default>
+          <GameStateComponent />
+        </Default>
+      </div>
+    </AppState.Provider>
   );
 }
 
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,17 @@
 import React, { Fragment } from 'react'
+import { AppState, STATES, ACTION_TYPES } from '../app/App';
+
+const createGameAction = () => ({
+  type: ACTION_TYPES.CHANGE_GAME_STATE,
+  gameState: STATES.PLAYER_CREATION,
+})
 
 export const Login = () => {
+  const { dispatch } = React.useContext(AppState);
+  const createGame = () => dispatch(createGameAction())
   return (
     <Fragment>
-      <button>Ich erstell mal n Spiel</button>
+      <button onClick={createGame}>Ich erstell mal n Spiel</button>
     </Fragment>
   )
 }
