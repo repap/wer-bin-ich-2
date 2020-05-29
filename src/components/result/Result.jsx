40d084930c8acf80329e49359dd578e190368189
Dennis Penning <penning.dennis@googlemail.com>
2020-05-29 06:50:59 +0200

    Add action to start another round

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -1,12 +1,17 @@
 import React, { Fragment } from 'react'
 import { PlayerList } from '../../presentations/playerList/PlayerList'
+import { gameStateChangeAction } from '../../actions/game';
+import { STATES } from '../../const/game';
+import { AppState } from '../app/App';
 
 export const Result = () => {
+  const { dispatch } = React.useContext(AppState);
+  const startGame = () => dispatch(gameStateChangeAction(STATES.GAME_PREPERATION))
   return (
     <Fragment>
       <h2>Resultate</h2>
       <PlayerList />
-      <button>Noch ne Runde!</button>
+      <button onClick={startGame}>Noch ne Runde!</button>
     </Fragment>
   )
 }
